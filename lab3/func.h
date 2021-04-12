b61d919c0a69bdabe447c0817b4348d98bc1828c
TestCheat123 <dp23092002@gmail.com>
2021-04-12 21:02:08 +0700

    Stack ready

--- lab3/func.h
+++ lab3/func.h
@@ -7,19 +7,35 @@ using namespace std;
 
 struct MyStack {
 	struct Node
-	{
+	{		
 		string name;
 		float cost;
 		int Value, storeValue;
 		Node* prev;
 	};
 
+	string StackName;
 	Node* Top = NULL;
-	int Count;
+	int Count = 0;
 	bool Push(string,float,int,int);
 	bool Pop(string&,float&,int&,int&);
-	bool SearchPop(string);
+	bool SearchPop(string, struct MyStack &Buff);
+	void Info();
+};
+
+struct MyQueue {
+	struct Node {
+		string name;
+		int value;
+		float cost; 
+	};
+
+	string QueueName;
+
+	bool Push(string, float, int, int);
+	bool Pop(string&, float&, int&, int&);
 	void Info();
 };
 
 void menu();
+void Choice(struct MyStack buff);
--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,27 +1,14 @@
-﻿//#include <iostream>
-#include "func.h"
+﻿#include "func.h"
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    string name = "Тыблоко", popName;
-    float cost = 10, popCost;
-    int Value = 30, storeValue = 100, popValue, popStoreValue;
-    bool check;
+    system("chcp 1251");
+ 
+    MyStack Pie;
+    Pie.StackName = "Пирожки Крутые";
+    Choice(Pie);
 
-    MyStack Apple;
-    check = Apple.Push(name, cost, Value, storeValue);
-    if(check)
-        Apple.Info();
-    check = Apple.Pop(popName,popCost,popValue,popStoreValue);
-    if (check)
-    {
-        cout << "\nУдаленный элемент: " << popName;
-        cout << "\nУдаленный элемент cost: " << popCost;
-        cout << "\nУдаленный элемент value: " << popValue;
-        cout << "\nУдаленный элемент store: " << popStoreValue;
-    }
-        
     return 0;
 }
 
